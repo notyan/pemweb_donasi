7b9126cb81052db40476b72f68e3e58ac04d3ea0
adafatya <aufaditya1895@gmail.com>
2021-05-04 13:39:10 +0700

    membentuk tabel relawan

--- database/migrations/2021_05_03_043422_create_relawan_table.php
+++ database/migrations/2021_05_03_043422_create_relawan_table.php
@@ -15,7 +15,25 @@ class CreateRelawanTable extends Migration
     {
         Schema::create('relawan', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('id_user');
+            $table->string('nama_depan');
+            $table->string('nama_belakang');
+            $table->string('alamat_ktp');
+            $table->string('no_wa');
+            $table->foreignId('id_provinsi');
+            $table->foreignId('id_kab');
+            $table->foreignId('id_kec');
+            $table->foreignId('id_kelurahan');
+            $table->foreignId('id_profesi');
+            $table->foreignId('id_jk');
+            $table->foreignId('id_agama');
+            $table->string('token');
+            $table->string('email')->unique();
+            $table->boolean('is_verified');
+            $table->timestamp('inserted_at');
+            $table->string('inserted_by');
+            $table->timestamp('edited_at');
+            $table->string('edited_by');
         });
     }
 
