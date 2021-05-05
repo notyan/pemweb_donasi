d826542ba93b0b89c673545f5fcd41dc4a6f0342
notyan <defaroyan@student.uns.ac.id>
2021-05-06 05:56:49 +0700

    Program Donatur Table

--- database/migrations/2021_05_03_043456_create_program_donatur_table.php
+++ database/migrations/2021_05_03_043456_create_program_donatur_table.php
@@ -14,8 +14,16 @@ class CreateProgramDonaturTable extends Migration
     public function up()
     {
         Schema::create('program_donatur', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('id');
+            $table->foreignId('id_program')->constrained('program');
+            $table->unsignedInteger('nominal_donasi');                   //APABILA DOLLAR PAKAI DOUBLE
+            $table->string('id_rekening',50);
+            $table->strubg('nama_pengirim',50);
+            $table->string('no_rekening_pengirim',50);
+            $table->strimg('nama_atas_nama',50);
+            $table->string('email');
+            $table->text('pesan');
+            $table->string('status');
         });
     }
 
