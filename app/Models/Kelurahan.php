45c3f4e6c1fd245e3f88949686d3ba393ea7d735
HizaCrenata <princespiniq@gmail.com>
2021-05-27 07:00:56 +0700

    Create Controller and Models for Kecamatan, Kelurahan and Rekening

--- app/Models/Kelurahan.php
+++ app/Models/Kelurahan.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Kelurahan extends Model
 {
     use HasFactory;
+
+    protected $table = 'Kelurahan';
+
+    public function id_kec(): BelongsTo
+    {
+        return $this->belongsTo(Kecamatan::class, 'id_kec');
+        
+    }
 }
--- app/Models/Rekening.php
+++ app/Models/Rekening.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Rekening extends Model
 {
     use HasFactory;
+
+    protected $table = 'Rekening';
+
+    public function id_vendor(): BelongsTo
+    {
+        return $this->belongsTo(ref_vendor_saving::class, 'id_vendor');
+    }
 }
