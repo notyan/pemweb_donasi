4a04baf8df217b9432dcf3b9eb9bb04f828f4c36
notyan <defaroyan@gmail.com>
2021-05-27 00:17:54 +0000

    merge

--- app/Models/Kabupaten.php
+++ app/Models/Kabupaten.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Kabupaten extends Model
 {
     use HasFactory;
+<<<<<<< HEAD
     protected $table = 'kabupaten';
+=======
+
+    protected $table = 'Kabupaten';
+>>>>>>> a5932c82e58c5e5b2aac1fe03b50f5ea4825ec32
 }
