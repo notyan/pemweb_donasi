7fb8585921737665eb3894073882584c9a13c5f3
adafatya <aufaditya1895@gmail.com>
2021-06-10 07:10:14 +0700

    on progress program controller

---
+++ app/Http/Controllers/ProgramController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class ProgramController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $list_berita = DB::table('program_berita')->get();
+
+        return view('program.index', compact('list_berita'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = DB::table('program')->where('id', $id)->get();
+
+        return view('program.detail', compact('data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Program  $program
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Program $program)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Program  $program
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Program $program)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Program  $program
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Program $program)
+    {
+        //
+    }
+}
