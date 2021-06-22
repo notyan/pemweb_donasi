3e7e35b4191ee4b64a19c113b00ca911d1b52845
adafatya <aufaditya1895@gmail.com>
2021-06-22 23:04:05 +0700

    add donation feature and fix table

--- resources/views/program/berita.blade.php
+++ resources/views/program/berita.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $data->judul }}</title>
+    <title>{{ $data_berita->judul }}</title>
 </head>
 <body>
-    {!! $data->konten_berita !!}
+    {!! $data_berita->konten_berita !!}
+    <div id="info">
+        <p>
+            <h3>Info tentang program</h3>
+            Nama Program : {{ $data_program->nama_program }}
+            Pemilik : {{ DB::table('users')->where('id', $data_program->id_user)->get()->first()->name }} <br />
+            Target : {{ $data_program->target }} <br />
+            Batas Akhir : {{ $data_program->batas_akhir }} <br />
+            <a href="{{ route('program.donasi', ['id' => $data_program->id]) }}">Donasi</a>
+        </p>
+    </div>
 </body>
 </html>
